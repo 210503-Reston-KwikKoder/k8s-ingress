ac5f889ecea2fdfb8ad89e57bc4ff0e012e69f4a
phobos42 <garrettruffner42@gmail.com>
2021-07-01 11:59:44 -0400

    trying some new things

--- ingress_setup/setup.sh
+++ ingress_setup/setup.sh
@@ -3,6 +3,8 @@
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
+helm repo update
+
 helm install nginx-ingress ingress-nginx/ingress-nginx \
    --namespace kwikkoder \
    --set controller.replicaCount=2 \
